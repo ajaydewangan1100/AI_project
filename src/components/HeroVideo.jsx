bdc477832f1b558dce0ea262acca48744157ab02
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-06-23 09:32:54 +0530

    now video is imported

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
+import MainVid from "../assets/hero_video.mp4"
 
 const HeroVideo = () => {
   const [searchText, setSearchText] = useState("");
 
-  const videoSrc = "src/assets/hero_video.mp4";
   return (
     <div className='relative w-full min-h-[100vh] overflow-hidden '>
 
       <div className='relative origin-center left-0 w-full min-h-100vh h-auto z-[-10] overflow-hidden flex justify-around content-end content-stretch'>
         <video className='fixed w-full min-h-100vh h-full object-cover' autoPlay loop muted>
-          <source src="src/assets/hero_video.mp4" type="video/mp4" />
+          <source src={MainVid} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         
@@ -21,7 +21,7 @@ const HeroVideo = () => {
             <input value={searchText} name='search' onChange={(e)=>{
               setSearchText(e.target.value)}} 
               className='p-2 px-5 min-w-[220px] w-[30vw] text-xl font-semibold text-gray-800 rounded-3xl opacity-20 focus:opacity-70 bg-gray-200 focus:bg-gray-100 focus:outline-none' placeholder='Search AI here' />
-            <button type="submit" class="" />
+            <button type="submit" className="" />
           </form>
         </div>
 
